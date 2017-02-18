098034998f9c707f9986aa797775bcc5fa39f9a4
cherriewang <cherrie.cherry@gmail.com>
2017-02-18 04:26:00 -0800

    trying to get calories from nutritionix

--- alexa/src/index.js
+++ alexa/src/index.js
@@ -21,6 +21,32 @@ var handlers = {
         var myItemName = myIntent.slots.ItemName.value;
         var speechOutput = 'I invoked the LogMultipleItemsIntent ' + myItemNum + ' ' + myItemName;
 
+        // call https request for the item name
+        var url = "https://api.nutritionix.com/v1_1/search/cheddar%20cheese?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=27d56daa&appKey=801497a4013af4e17085d5d46e305d0e";
+        var myItemCalorie = httpGetAsync(url, callback);
         this.emit(':tell', speechOutput);
     }
 };
+
+
+//https://api.nutritionix.com/v1_1/search/cheddar%20cheese?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=27d56daa&appKey=801497a4013af4e17085d5d46e305d0e
+//ID: 27d56daa
+//KEY: 801497a4013af4e17085d5d46e305d0e
+var httpGetAsync = function(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            return callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+var callback = function(e) {
+    var response = JSON.parse(xhr.responseText);
+    alert(response.ip);
+    return response['hits'][0]['nf_calories']; // should return the calories
+}
+
+
+
