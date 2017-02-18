2851dec48c68432865c18da580376e57b09defff
Brandon Cen <bcen96@gmail.com>
2017-02-18 09:36:08 -0800

    howAmIDoing

--- alexa/src/index.js
+++ alexa/src/index.js
@@ -12,6 +12,9 @@ var handlers = {
         this.emit(':tell', 'Launching');
     },
     'LogItemIntent': function () {
+        /*
+        Intended to calculate the caloric count of an item, then add it as an item to our database
+        */
         var speechOutput = 'I invoked the LogItemIntent ' + this.event.request.intent.slots.ItemName.value;
         this.emit(':tell', speechOutput);
     },
@@ -23,7 +26,16 @@ var handlers = {
 
         // call https request for the item name
         var url = "https://api.nutritionix.com/v1_1/search/cheddar%20cheese?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=27d56daa&appKey=801497a4013af4e17085d5d46e305d0e";
-        var myItemCalorie = httpGetAsync(url, callback);
+        //var myItemCalorie = httpGetAsync(url, callback);
+        this.emit(':tell', speechOutput);
+    },
+    'HowAmIDoingIntent': function () {
+        /*
+        Intended to retrieve data from dynamodb, then send that data for alexa to reply
+        */
+
+
+        var speechOutput = 'I invoked the HowAmIDoingIntent';
         this.emit(':tell', speechOutput);
     }
 };
